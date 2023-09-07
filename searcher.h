0a318894b6d2f3aeb33a47215c70525c8ba1fa73
sal <salvadorbu@vt.edu>
2023-09-07 14:58:32 -0400

    formatting fix

--- searcher.h
+++ searcher.h
@@ -4,7 +4,7 @@ typedef struct search_base
 {
     char* in; //mmap pointer
     char* target; //user search pattern
-    int target_length;
+    int target_length; // # of chars in pattern
     int buffer; //size of the search region
     DoublyLinkedList* result_list;
 } search_base;
